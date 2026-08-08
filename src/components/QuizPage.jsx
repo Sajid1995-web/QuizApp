989bcf313131852ea02211fefe49bd4bea1f92ea
Sajid1995-web <sajid11449@gmail.com>
2026-08-08 13:02:50 +0530

    Update QuizPage.jsx

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef, useCallback } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
-const API_BASE = "http://localhost:3000";
+const API_BASE = "https://quizappbackend-k09m.onrender.com";
 
 const shuffleArray = (array) => {
   const newArray = [...array];
