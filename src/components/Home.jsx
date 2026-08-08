f34545440dda88386547a9c078f7247c71c6fd54
Sajid1995-web <sajid11449@gmail.com>
2026-08-08 21:48:49 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -317,7 +317,7 @@ const styles = {
   mainTitle: {
     fontSize: "45px", 
     fontWeight: "800",
-    color: "#1a1a40",
+    color: "#1D526C",
     margin: "0 0 12px 0",
     lineHeight: "1.3",
   },
